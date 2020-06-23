9a3171526933faca7c8fbe933583990e1b497ca3
BrunoO <brunojo.orlandy@hotmail.com>
2020-06-23 20:37:35 -0300

    commit 1

---
+++ Genomium/Genomium/Forms/MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Genomium
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.gvDados = new System.Windows.Forms.DataGridView();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gene = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Familia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PapelBiologico = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PosiçãoInicial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PosiçãoFinal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SequenciaDNA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gvDados
+            // 
+            this.gvDados.AllowUserToAddRows = false;
+            this.gvDados.AllowUserToOrderColumns = true;
+            this.gvDados.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvDados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nome,
+            this.Gene,
+            this.Tipo,
+            this.Familia,
+            this.PapelBiologico,
+            this.PosiçãoInicial,
+            this.PosiçãoFinal,
+            this.SequenciaDNA});
+            this.gvDados.Location = new System.Drawing.Point(12, 41);
+            this.gvDados.Name = "gvDados";
+            this.gvDados.ReadOnly = true;
+            this.gvDados.Size = new System.Drawing.Size(737, 459);
+            this.gvDados.TabIndex = 0;
+            this.gvDados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvDados_CellContentClick);
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Nome.DataPropertyName = "Nome";
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            this.Nome.ReadOnly = true;
+            // 
+            // Gene
+            // 
+            this.Gene.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Gene.DataPropertyName = "Gene";
+            this.Gene.HeaderText = "Gene";
+            this.Gene.Name = "Gene";
+            this.Gene.ReadOnly = true;
+            // 
+            // Tipo
+            // 
+            this.Tipo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Tipo.DataPropertyName = "Tipo";
+            this.Tipo.HeaderText = "Tipo";
+            this.Tipo.Name = "Tipo";
+            this.Tipo.ReadOnly = true;
+            // 
+            // Familia
+            // 
+            this.Familia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Familia.DataPropertyName = "Familia";
+            this.Familia.HeaderText = "Família";
+            this.Familia.Name = "Familia";
+            this.Familia.ReadOnly = true;
+            // 
+            // PapelBiologico
+            // 
+            this.PapelBiologico.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.PapelBiologico.DataPropertyName = "PapelBiologico";
+            this.PapelBiologico.HeaderText = "Papel Biológico";
+            this.PapelBiologico.Name = "PapelBiologico";
+            this.PapelBiologico.ReadOnly = true;
+            // 
+            // PosiçãoInicial
+            // 
+            this.PosiçãoInicial.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.PosiçãoInicial.DataPropertyName = "PosicaoInicial";
+            this.PosiçãoInicial.HeaderText = "Posição Inicial";
+            this.PosiçãoInicial.Name = "PosiçãoInicial";
+            this.PosiçãoInicial.ReadOnly = true;
+            // 
+            // PosiçãoFinal
+            // 
+            this.PosiçãoFinal.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.PosiçãoFinal.DataPropertyName = "PosicaoFinal";
+            this.PosiçãoFinal.HeaderText = "Posição Final";
+            this.PosiçãoFinal.Name = "PosiçãoFinal";
+            this.PosiçãoFinal.ReadOnly = true;
+            // 
+            // SequenciaDNA
+            // 
+            this.SequenciaDNA.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.SequenciaDNA.DataPropertyName = "SequenciaDNA";
+            this.SequenciaDNA.HeaderText = "Sequencia DNA";
+            this.SequenciaDNA.Name = "SequenciaDNA";
+            this.SequenciaDNA.ReadOnly = true;
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImportar.Location = new System.Drawing.Point(110, 505);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(66, 23);
+            this.btnImportar.TabIndex = 3;
+            this.btnImportar.Text = "Importar";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExportar.Location = new System.Drawing.Point(12, 505);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(75, 23);
+            this.btnExportar.TabIndex = 4;
+            this.btnExportar.Text = "Exportar";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnInfo.Location = new System.Drawing.Point(12, 12);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(53, 23);
+            this.btnInfo.TabIndex = 8;
+            this.btnInfo.Text = "Info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 532);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnImportar);
+            this.Controls.Add(this.gvDados);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(500, 200);
+            this.Name = "MainForm";
+            this.Text = "Genomium";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvDados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gvDados;
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gene;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Familia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PapelBiologico;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PosiçãoInicial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PosiçãoFinal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SequenciaDNA;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.Button btnInfo;
+    }
+}
+
---
+++ Genomium/Genomium/Forms/MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Genomium
+{
+    public partial class MainForm : Form
+    {
+        List<Organismo> Organismos = new List<Organismo>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "CSV|*.csv|txt (*.txt)|*.txt|All files (*.*)|*.*", ValidateNames = true, Multiselect = false })
+                {
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                        ProcessarCSV(openFileDialog.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void ProcessarCSV(string caminho)
+        {
+            string[] linhas = System.IO.File.ReadAllLines(caminho);
+
+
+            Parallel.ForEach(linhas, new ParallelOptions() { MaxDegreeOfParallelism = 8 }, linha =>
+                    {
+                        string[] dados = linha.Split(';');
+                        Organismos.Add(new Organismo(dados[0], dados[1], dados[2], dados[3], dados[4], Convert.ToInt32(dados[5]), Convert.ToInt32(dados[6]), dados[7]));
+                    });
+
+            this.gvDados.AutoGenerateColumns = false;
+            this.gvDados.DataSource = Organismos;
+
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void lblAlunos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            string title = "Sobre";
+            string texto = "Trabalho de Fundamentos de Sistemas Operacionais\n" +
+                "Alunos :  Bruno Jonatha Orlandy\n" +
+                "Jessica Da Silva Pavan\n" +
+                "Vinicius Molon Toigo\n" +
+                "Codigo fonte : https://github.com/ViniciusMToigo/Genomium.git";
+            MessageBox.Show(texto, title);
+        }
+
+        private void gvDados_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            string filename = "";
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "CSV (*.csv)|*.csv|txt (*.txt)|*.txt";
+            sfd.FileName = "OutputGenoma.csv";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Os dados serão exportados e você será notificado quando estiver pronto.");
+                if (File.Exists(filename))
+                {
+                    try
+                    {
+                        File.Delete(filename);
+                    }
+                    catch (IOException ex)
+                    {
+                        MessageBox.Show("Não foi possível gravar os dados no disco." + ex.Message);
+                    }
+                }
+                int columnCount = gvDados.ColumnCount;
+                string columnNames = "";
+                string[] output = new string[gvDados.RowCount + 1];
+                for (int i = 0; i < columnCount; i++)
+                {
+                    columnNames += gvDados.Columns[i].Name.ToString() + ";";
+                }
+                output[0] += columnNames;
+                for (int i = 1; (i - 1) < gvDados.RowCount; i++)
+                {
+                    for (int j = 0; j < columnCount; j++)
+                    {
+                        //output[i] += dataGridView.Rows[i - 1].Cells[j].Value.ToString() + ",";
+                        if (gvDados.Rows[i - 1].Cells[j].Value != null) output[i] += gvDados.Rows[i - 1].Cells[j].Value.ToString() + ";";
+                    }
+                }
+                System.IO.File.WriteAllLines(sfd.FileName, output, System.Text.Encoding.UTF8);
+                MessageBox.Show("Arquivo foi gerado e está pronto para uso.");
+            }
+        }
+    }
+}
